38a7860d34fbaaff73963edd28bb5a23f577b6c6
Thomas <dahlg136@morris.umn.edu>
2021-09-07 20:08:33 -0500

    Add regex.sh that passes tests
    This regex.sh formats the text files the way we want them.

--- regex/regex.sh
+++ regex/regex.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-file=$1" | sed 's/\[_input.text]//'`
-echo $file
-awk 'match($0, /([a-zA-Z]+), ([a-zA-Z]+)/, groups) {print "1. " groups[1] "\n" "2. " groups[2] "\n" }' $1 > $1.txt
+sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' < r0_input.txt > r0_output.txt
 
-awk 'match($0, /(\w+), (\w+)/, groups) {print "1. " groups[1] "\n" "2. " groups[2] "\n" }' < $1
+#Only prints lines that match this criteria
+sed -En 's/\* I am ([A-Za-z]+). My favorite sandwich is ([a-zA-Z]+)./1. \1\n2. \2\n/p' < r1_input.txt > r1_output.txt
 
-awk 'match($0, /([[:alpha:]]+), ([[:alpha:]]+)/, groups) {print "1. " groups[1] "\n" "2. " groups[2] "\n" }' < $1
+sed -E 's/\* sandwich with ([a-zA-Z\.]+) ([a-zA-Z ]+)/1. \1\n2. \2\n/' < r2_input.txt > r2_output.txt
